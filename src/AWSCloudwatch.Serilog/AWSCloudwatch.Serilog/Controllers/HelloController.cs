27e28088567088cdd5b2cca4666fe20a6dee6b07
mirusser <mirusser@gmail.com>
2023-01-18 14:48:06 +0100

    Use Serilog with AWS Cloudwatch Demo

---
+++ src/AWSCloudwatch.Serilog/AWSCloudwatch.Serilog/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AWSCloudwatch.Serilog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    private readonly ILogger<HelloController> logger;
+
+    public HelloController(ILogger<HelloController> logger)
+    {
+        this.logger = logger;
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult Get(int id)
+    {
+        logger.LogDebug("Received request with is as {id}", id);
+        logger.LogInformation("Processing your request");
+        logger.LogError("Some errors occured");
+
+        return Ok("Logged");
+    }
+}
