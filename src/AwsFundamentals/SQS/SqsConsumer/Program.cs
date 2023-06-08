94dee7f38ff620a21b92408ccac295c1313dda10
mirusser <mirusser@gmail.com>
2023-06-08 19:31:49 +0200

    AWS - simple sqs consumer

---
+++ src/AwsFundamentals/SQS/SqsConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+AmazonSQSClient sqsClient = new();
+
+GetQueueUrlResponse queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+ReceiveMessageRequest receiveMessageRequest = new()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string> { "ALL" },
+    MessageAttributeNames = new List<string> { "ALL" }
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(3000);
+}
+
+Console.ReadKey();
