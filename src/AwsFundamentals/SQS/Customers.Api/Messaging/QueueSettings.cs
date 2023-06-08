86a1e21d1742d3c2273acffe97c215380cbceb8c
mirusser <mirusser@gmail.com>
2023-06-08 19:32:05 +0200

    AWS - web api - publisher demo

---
+++ src/AwsFundamentals/SQS/Customers.Api/Messaging/QueueSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Customers.Api.Messaging;
+
+public sealed class QueueSettings
+{
+    public const string Key = "Queue";
+    public required string Name { get; init; }
+}
---
+++ src/AwsFundamentals/SQS/Customers.Api/Messaging/SqsMessenger.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+
+namespace Customers.Api.Messaging;
+
+public sealed class SqsMessenger : ISqsMessenger
+{
+    private readonly IAmazonSQS sqs;
+    private readonly IOptions<QueueSettings> queueSettings;
+    private string? queueUrl;
+
+    public SqsMessenger(
+        IAmazonSQS sqs,
+        IOptions<QueueSettings> queueSettings)
+    {
+        this.sqs = sqs;
+        this.queueSettings = queueSettings;
+    }
+
+    public async Task<SendMessageResponse> SendMessageAsync<T>(T request)
+    {
+        var queueUrl = await GetQueueUrlAsync<T>();
+
+        var sendMessageRequest = new SendMessageRequest()
+        {
+            QueueUrl = queueUrl,
+            MessageBody = JsonSerializer.Serialize(request),
+            MessageAttributes = new()
+            {
+                {
+                    "MessageType",
+                    new MessageAttributeValue
+                    {
+                        DataType = "String",
+                        StringValue = typeof(T).Name
+                    }
+                }
+            }
+        };
+
+        return await sqs.SendMessageAsync(sendMessageRequest);
+    }
+
+    private async Task<string?> GetQueueUrlAsync<T>()
+    {
+        if (string.IsNullOrEmpty(queueUrl))
+        {
+            GetQueueUrlResponse queueUrlResponse =
+                await sqs.GetQueueUrlAsync(queueSettings.Value.Name);
+
+            if (!string.IsNullOrEmpty(queueUrlResponse.QueueUrl))
+            {
+                queueUrl = queueUrlResponse.QueueUrl;
+            }
+        }
+
+        return queueUrl;
+    }
+}
