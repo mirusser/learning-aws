f7e4cec20ee45c7db86b4d2cf0dbdd2c7aafa9dc
mirusser <mirusser@gmail.com>
2023-06-08 15:56:10 +0200

    AWS - SQS - simple message publisher

---
+++ src/AwsFundamentals/SQS/SqsPublisher/CustomerCreated.cs
@@ -0,0 +1,10 @@
+﻿namespace SqsPublisher;
+
+public sealed class CustomerCreated
+{
+    public required Guid Id { get; init; }
+    public required string FullName { get; init; }
+    public required string Email { get; init; }
+    public required string GitHubUserName { get; init; }
+    public required DateTime DateOfBirth { get; init; }
+}
---
+++ src/AwsFundamentals/SQS/SqsPublisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+AmazonSQSClient sqsClient = new();
+
+CustomerCreated customer = new()
+{
+    Id = Guid.NewGuid(),
+    Email = "example@example.com",
+    FullName = "John Doe",
+    DateOfBirth = new DateTime(1990, 1, 1),
+    GitHubUserName = "JohnDoe"
+};
+
+GetQueueUrlResponse queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+SendMessageRequest sendMessageRequest = new()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new()
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response);
+
+Console.ReadKey();
