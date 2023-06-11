4b2476783cf8811317064086a45b67ac6e89a202
mirusser <mirusser@gmail.com>
2023-06-11 22:51:46 +0200

    AWS - simple SNS Publisher (demo)

---
+++ src/AwsFundamentals/SNS/SnsPublisher/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text.Json;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+
+CustomerCreated customer = new ()
+{
+    Id = Guid.NewGuid(),
+    Email = "example@example.com",
+    FullName = "John Doe",
+    DateOfBirth = new DateTime(1990, 1, 1),
+    GitHubUsername = "JohnDoe"
+};
+
+AmazonSimpleNotificationServiceClient snsClient = new();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+PublishRequest publishRequest = new()
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new()
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
+
+Console.WriteLine(response);
+
+Console.ReadLine();
