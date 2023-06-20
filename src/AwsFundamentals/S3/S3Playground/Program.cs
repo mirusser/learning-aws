d7ead3ff0e105736dcbecdba30962f922a0b5eeb
mirusser <mirusser@gmail.com>
2023-06-20 20:53:14 +0200

    AWS - S3 - simple upload - demo

---
+++ src/AwsFundamentals/S3/S3Playground/Program.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+await SimpleUpload();
+
+static async Task SimpleUpload()
+{
+    var s3Client = new AmazonS3Client();
+
+    await using var inputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+
+    var putObjectRequest = new PutObjectRequest
+    {
+        BucketName = "awscoursefromnick",
+        Key = "images/face.jpg",
+        ContentType = "image/jpeg",
+        InputStream = inputStream
+    };
+
+    await s3Client.PutObjectAsync(putObjectRequest);
+}
