acab53178f3c9ff2fd865253375a285ea02e8ff1
mirusser <mirusser@gmail.com>
2023-06-20 21:07:58 +0200

    AWS - S3 - simple download demo

--- src/AwsFundamentals/S3/S3Playground/Program.cs
+++ src/AwsFundamentals/S3/S3Playground/Program.cs
@@ -1,19 +1,44 @@
-﻿using Amazon.S3;
+﻿using System.Text;
+using Amazon.S3;
 using Amazon.S3.Model;
 
 await SimpleUpload();
+await SimpleDownload();
+
+Console.ReadLine();
+
+static async Task SimpleDownload()
+{
+    var s3Client = new AmazonS3Client();
+
+    var getObjectRequest = new GetObjectRequest
+    {
+        BucketName = "awscoursefromnick",
+        Key = "files/movies.csv",
+    };
+
+    var response = await s3Client.GetObjectAsync(getObjectRequest);
+
+    await using var memoryStream = new MemoryStream();
+    response.ResponseStream.CopyTo(memoryStream);
+
+    var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+    //Console.WriteLine(text);
+    await Console.Out.WriteLineAsync(text);
+}
 
 static async Task SimpleUpload()
 {
     var s3Client = new AmazonS3Client();
 
-    await using var inputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+    await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
 
     var putObjectRequest = new PutObjectRequest
     {
         BucketName = "awscoursefromnick",
-        Key = "images/face.jpg",
-        ContentType = "image/jpeg",
+        Key = "files/movies.csv",
+        ContentType = "text/csv",
         InputStream = inputStream
     };
 
