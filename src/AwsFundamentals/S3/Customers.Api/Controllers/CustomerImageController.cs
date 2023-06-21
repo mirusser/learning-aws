b300854358465e05a9e6eee11f8eb4ecabc1076b
mirusser <mirusser@gmail.com>
2023-06-21 20:25:40 +0200

    AWS- S3 - File upload via api (demo)

---
+++ src/AwsFundamentals/S3/Customers.Api/Controllers/CustomerImageController.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using Customers.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customers.Api.Controllers;
+
+[ApiController]
+public class CustomerImageController : ControllerBase
+{
+    private readonly ICustomerImageService _customerImageService;
+
+    public CustomerImageController(ICustomerImageService customerImageService)
+    {
+        _customerImageService = customerImageService;
+    }
+
+    [HttpPost("customers/{id:guid}/image")]
+    public async Task<IActionResult> Upload(
+        [FromRoute] Guid id,
+        [FromForm(Name = "Data")] IFormFile file)
+    {
+        var response = await _customerImageService.UploadImageAsync(id, file);
+
+        if (response.HttpStatusCode == HttpStatusCode.OK)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest(response);
+    }
+
+    [HttpGet("customers/{id:guid}/image")]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("customers/{id:guid}/image")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/AwsFundamentals/S3/Customers.Api/Services/CustomerImageService.cs
@@ -0,0 +1,45 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Customers.Api.Services;
+
+public class CustomerImageService : ICustomerImageService
+{
+    private readonly IAmazonS3 s3;
+
+    //for simplicity sake, but should be in configuration
+    private readonly string bucketName = "awscoursefromnick";
+
+    public CustomerImageService(IAmazonS3 s3)
+    {
+        this.s3 = s3;
+    }
+
+    public Task<DeleteObjectResponse> DeleteImageAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetObjectResponse> GetImageResponse(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<PutObjectResponse> UploadImageAsync(Guid id, IFormFile formFile)
+    {
+        var putObjectRequest = new PutObjectRequest
+        {
+            BucketName = bucketName,
+            Key = $"image/{id}",
+            ContentType = formFile.ContentType,
+            InputStream = formFile.OpenReadStream(),
+            Metadata =
+            {
+                ["x-amz-meta-originalname"] = formFile.FileName,
+                ["x-amz-meta-extension"] = Path.GetExtension(formFile.FileName)
+            }
+        };
+
+        return await s3.PutObjectAsync(putObjectRequest);
+    }
+}
