67f3f92094ce42f3f6a53277da3b2981cd97c96c
mirusser <mirusser@gmail.com>
2023-06-22 20:49:16 +0200

    AWS - SecretManager - simple retrieval of a secret

---
+++ src/AwsFundamentals/SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var listSecretVersionsRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionsRequest);
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey",
+    //VersionStage = "AWSPREVIOUS"
+};
+
+var response = await secretsManagerClient.GetSecretValueAsync(request);
+
+Console.WriteLine(response.SecretString);
+Console.ReadLine();
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+
+Console.WriteLine(describeResponse);
+Console.ReadLine();
